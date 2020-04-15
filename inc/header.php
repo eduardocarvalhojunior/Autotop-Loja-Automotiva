5b8b8416792b89141f83c7f65d8388f59a042443
eduardocarvalhojunior <junior.baller@hotmail.com>
2020-04-14 23:28:14 -0300

    adicionando navebar

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,27 @@
 <body>
-      <header>
-          
-      </header>
-
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="index.php"> 
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fautotopmultimarcasrs%2F&psig=AOvVaw3W-pwlKpevoiydqeQ5v04B&ust=1587001809716000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCODT2suo6egCFQAAAAAdAAAAABAD" width="100" height="120">
+                Auto Top</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Especialidades </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Dúvidas </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php require_once("./inc/head.php"); ?>
 <?php require_once("./inc/header.php"); ?>
-
-
 <?php require_once("./inc/footer.php"); ?>
    
