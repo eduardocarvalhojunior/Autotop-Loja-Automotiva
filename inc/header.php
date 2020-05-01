35ece3f1ecfe4a4cceb387136cd9cb0c86682aa0
eduardocarvalhojunior <junior.baller@hotmail.com>
2020-04-30 23:36:15 -0300

    ajustes de estilos

--- inc/header.php
+++ inc/header.php
@@ -1,30 +1,36 @@
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="index.php"> 
-                <img src="" width="100" height="120">
-                Auto Top</a>
+        <nav class="navbar navbar-expand-lg navbar bg-white fixed-top">
+            <a class="navbar-brand" href="index.php">
+                <img src="img/logo.jpg" width="140" height="120">Auto Top</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Especialidades </a>
+                        <a class="nav-link" href="#">Especialidades</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"> Dúvidas </a>
+                        <a class="nav-link" href="#">Dúvidas</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"data-toggle="modal" data-target="#modalLogin">Login </a>
+                        <a class="nav-link" href="#" data-toggle="modal" data-target="#modalLogin">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contato</a>
+                        <a class="nav-link" href="#">Contato</a>
                     </li>
                 </ul>
+                <style>
+                    .navbar-nav {
+                        font-size: 20px;
+                    }
+
+                </style>
+
             </div>
         </nav>
     </header>
--- index.php
+++ index.php
@@ -13,22 +13,22 @@
     <div class="carousel-item active">
       <img src="img/bmw.jpeg" class="d-block w-100" width="" height="" alt="">
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <h5>Não tenha medo</h5>
+        <p>Se Surpreenda com o novo</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="img/urus.jpeg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <h5>A escolha é sua</h5>
+        <p>Acredite, arrisque e siga em frente</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="img/lexus.jpeg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        <h5>Você está no comando</h5>
+        <p>Não deixo nada para o amanhã</p>
       </div>
     </div>
   </div>
@@ -43,12 +43,13 @@
 </div>
 <main class="container" class=row>
 <section in=serviços>
-<h1 class="col-12 text-center text-info mt-5 mb-3"> Serviços </h1>
+<h1 class="col-12 text-center text-info mt-5 mb-3">Serviços</h1>
+
 <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="img/aparelho.jpeg" alt="Imagem de capa do card">
     <div class="card-body">
-      <h5 class="card-title">Análise do Sistema de Injeção</h5>
+      <h5 class="card-title" style="text-decoration: underline; text-align: center">Análise do Sistema de Injeção</h5>
       <p class="card-text">Fazemos análise e diagnóstico do sistema de injeção eletrônica completo.</p>
       <p class="card-text"><small class="text-muted">Tempo médio de serviço: 2 horas. </small></p> 
     </div>
@@ -56,7 +57,7 @@
   <div class="card">
     <img class="card-img-top" src="img/correia.jpeg" alt="Imagem de capa do card">
     <div class="card-body">
-      <h5 class="card-title">Troca de Correia Dentada</h5>
+      <h5 class="card-title" style="text-decoration: underline; text-align: center">Troca de Correia Dentada</h5>
       <p class="card-text">Troca da correia dentada e rolamento tensor.</p>
       <p class="card-text"><small class="text-muted">Tempo médio de Serviço: 1 hora e 30 minutos.</small></p>
     </div>
@@ -64,18 +65,18 @@
   <div class="card">
     <img class="card-img-top" src="img/preventiva.jpeg" alt="Imagem de capa do card">
     <div class="card-body">
-      <h5 class="card-title">Manutenção Preventiva</h5>
-      <p class="card-text"> Marque sua manutenção preventiva alguns dias antes de pegar a estrada.</p>
+      <h5 class="card-title" style="text-decoration: underline; text-align: center">Manutenção Preventiva</h5>
+      <p class="card-text">Marque sua manutenção preventiva alguns dias antes de pegar a estrada.</p>
       <p class="card-text"><small class="text-muted">Tempo médio de serviço: 4 horas</small></p>
     </div>
   </div>
 </div>
-<h1 class="col-12 text-center text-info mt-5 mb-3"> Especialidades </h1>
+<h1 class="col-12 text-center text-info mt-5 mb-3">Especialidades</h1>
 <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="img/analise.jpeg" alt="Imagem de capa do card">
     <div class="card-body">
-      <h5 class="card-title">Manutenção do Sistema Partida</h5>
+      <h5 class="card-title" style="text-decoration: underline; text-align: center">Manutenção do Sistema Partida</h5>
       <p class="card-text">Teste e correção de todo o sistema elétrico de iginição e combustão.</p>
       <p class="card-text"><small class="text-muted">Tempo médio de serviço: 3 horas.</small></p>
     </div>
@@ -83,7 +84,7 @@
   <div class="card">
     <img class="card-img-top" src="img/manutencao.jpeg" alt="Imagem de capa do card">
     <div class="card-body">
-      <h5 class="card-title">Limpeza do Sistema de Injeção de Combustivel</h5>
+      <h5 class="card-title" style="text-decoration: underline; text-align: center">Limpeza do Sistema de Injeção</h5>
       <p class="card-text"> Manutenção preventiva e corretiva do sistema de injeção de combustível.</p>
       <p class="card-text"><small class="text-muted">Tempo médio de serviço: 2 horas.</small></p>
     </div>
@@ -91,7 +92,7 @@
   <div class="card">
     <img class="card-img-top" src="img/injeção.jpeg" alt="Imagem de capa do card">
     <div class="card-body">
-      <h5 class="card-title">Correção dos Sistemas Elétricos</h5>
+      <h5 class="card-title" style="text-decoration: underline; text-align: center,">Correção dos Sistemas Elétricos</h5>
       <p class="card-text">É importante também a parte mecanica do carro a parte elétrica em dia evita que você fique na mão.</p>
       <p class="card-text"><small class="text-muted">Tempo médio de serviço: 3 horas.</small></p>
     </div>
